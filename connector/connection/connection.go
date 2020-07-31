1bd5949574255f7417972858590b78cc644751a2
Wendell Nichols <wnichols@tibco.com>
2020-07-31 08:51:16 -0600

    tls updates

--- connector/connection/connection.go
+++ connector/connection/connection.go
@@ -1,11 +1,17 @@
 package connection
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 
 	"github.com/apache/pulsar-client-go/pulsar"
 	"github.com/project-flogo/core/data/metadata"
 	"github.com/project-flogo/core/support/connection"
+	"github.com/prometheus/common/log"
 )
 
 func init() {
@@ -16,15 +22,15 @@ func init() {
 
 // Settings comment
 type Settings struct {
-	URL                  string `md:"url,required"`
-	AthenzAuthentication string `md:"athenzauth"`
+	Name     string `md:"name,required"`
+	URL      string `md:"url,required"`
+	CaCert   string `md:"cacert"`
+	CertFile string `md:"certFile"`
+	KeyFile  string `md:"keyFile"`
 }
 
 // JWTToken             string `md:"jwttoken"`
 // AthenzAuthentication string `md:"athenzauth"`
-// CertFile             string `md:"certFile"`
-// KeyFile              string `md:"keyFile"`
-// CaCert               string `md:"cacert"`
 
 // PulsarConnection comment
 type PulsarConnection struct {
@@ -48,8 +54,11 @@ func (*Factory) NewManager(settings map[string]interface{}) (connection.Manager,
 		return nil, err
 	}
 
-	auth := getAuthentication(s)
-
+	auth, err := getAuthentication(s)
+	if err != nil {
+		return nil, err
+	}
+	log.Debugf("NewManager ")
 	clientOps := pulsar.ClientOptions{
 		URL:            s.URL,
 		Authentication: auth,
@@ -90,14 +99,82 @@ func (p *PulsarConnection) ReleaseConnection(connection interface{}) {
 
 }
 
-func getAuthentication(s *Settings) pulsar.Authentication {
-	if s.AthenzAuthentication != "" {
-		return nil
-		//return pulsar.NewAuthenticationAthenz(s.AthenzAuthentication)
+func getAuthentication(s *Settings) (auth pulsar.Authentication, err error) {
 
+	keystoreDir, err := createTempKeystoreDir(s)
+	if err != nil {
+		return
 	}
-	// if s.CertFile != "" && s.KeyFile != "" {
-	// 	return pulsar.NewAuthenticationTLS(s.CertFile, s.KeyFile)
-	// }
-	return nil
+	log.Debugf("Pulsar getAuthentication keystoreDir: %s", keystoreDir)
+	if keystoreDir == "" {
+		return nil, nil
+	}
+	auth = pulsar.NewAuthenticationTLS(keystoreDir+string(os.PathSeparator)+"certfile.pem",
+		keystoreDir+string(os.PathSeparator)+"keyfile.pem")
+	return
+}
+
+func createTempKeystoreDir(s *Settings) (keystoreDir string, err error) {
+	var certObj, keyObj map[string]interface{}
+	if s.CertFile == "" || s.KeyFile == "" {
+		log.Debugf("Pulsar::createTempKeystoreDir Have certFile and keyFile")
+		return "", nil
+	}
+	err = json.Unmarshal([]byte(s.CertFile), &certObj)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal([]byte(s.KeyFile), &keyObj)
+	if err != nil {
+		return
+	}
+
+	certBytes, err := getBytesFromFileSetting(certObj)
+	if err != nil {
+		return
+	}
+	keyBytes, err := getBytesFromFileSetting(keyObj)
+	if err != nil {
+		return
+	}
+	keystoreDir, err = ioutil.TempDir(os.TempDir(), s.Name)
+	if err != nil {
+		return
+	}
+	err = ioutil.WriteFile(keystoreDir+string(os.PathSeparator)+"certfile.pem", certBytes, 0644)
+	if err != nil {
+		return
+	}
+	err = ioutil.WriteFile(keystoreDir+string(os.PathSeparator)+"keyfile.pem", keyBytes, 0644)
+	if err != nil {
+		return
+	}
+	log.Debugf("Pulsar::createTempKeystoreDir Created folder %s", keystoreDir)
+	return
+}
+
+func getBytesFromFileSetting(fileSetting map[string]interface{}) (destArray []byte, err error) {
+	var header = "base64,"
+	value := fileSetting["content"].(string)
+	if value == "" {
+		return nil, fmt.Errorf("file based setting contains no data")
+	}
+
+	if strings.Index(value, header) >= 0 {
+		value = value[strings.Index(value, header)+len(header):]
+		decodedLen := base64.StdEncoding.DecodedLen(len(value))
+		destArray := make([]byte, decodedLen)
+		actualen, err := base64.StdEncoding.Decode(destArray, []byte(value))
+		if err != nil {
+			return nil, fmt.Errorf("file based setting not base64 encoded: [%s]", err)
+		}
+		if decodedLen != actualen {
+			newDestArray := make([]byte, actualen)
+			copy(newDestArray, destArray)
+			destArray = newDestArray
+			return newDestArray, nil
+		}
+		return destArray, nil
+	}
+	return nil, fmt.Errorf("internal error; file based setting not formatted correctly")
 }
